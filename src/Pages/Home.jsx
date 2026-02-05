ef3a9e6a32a6db9319035942347048ee85f48fd1
poojaS212 <pooja@propstory.com>
2026-02-05 12:51:03 +0530

    added navbar and herosection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,16 @@ import HowWeWork from "../Components/HowWeWork";
 import AboutSecond from "../Components/AboutSecond";
 import AboutScrollSection from "../Components/AboutStickyScroll";
 import AboutUs from "../Components/AboutUS";
+import HeroSection from "../Components/HeroSection";
+import Navbar from "../Components/Navbar";
 
 
  const Home = () => {
   return (
    <>
+    <div className="pt-18"></div>
+    <Navbar />
+    <HeroSection />
     <HowWeWork />
     <AboutUs />
     <AboutScrollSection />
